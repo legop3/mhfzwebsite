667337547d07b231673d6c0f47217e2450351b57
legop3 <minecratfd8@gmail.com>
2022-04-13 00:07:16 -0400

    sidebar

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/www/index.html');
-  });
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + 'www');
+//   });
+
+
+app.use(express.static(path.join(__dirname, 'www'))); 
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
